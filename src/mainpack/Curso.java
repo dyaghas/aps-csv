2ce5053477795a79554edcd1c4d2cff5d5678205
Caio Elias <66191663+CaioCElias@users.noreply.github.com>
2023-04-19 00:55:49 -0300

    começando classe Curso

--- src/mainpack/Curso.java
+++ src/mainpack/Curso.java
@@ -1,4 +1,39 @@
 package mainpack;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Curso {
+    private String nome;
+    private String nivel;
+    private int ano;
+
+    public Curso(String nome, String nivel, int ano) {
+        this.nome = nome;
+        this.nivel = nivel;
+        this.ano = ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public int getAnoDeEntradaDoCurso() {
+        return ano;
+    }
+
+    public void cadastrarCurso() throws IOException {
+        FileWriter writer = new FileWriter("./cursos.csv", true);
+        writer.append(nome);
+        writer.append(",");
+        writer.append(nivel);
+        writer.append(",");
+        writer.append(String.valueOf(ano));
+        writer.append("\n");
+        writer.close();
+    }
 }
