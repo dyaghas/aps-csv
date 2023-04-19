cc4f35f4b51f79ecc88626f383f9c49b718b57f9
Gustavo Cristian <diamonddyaghas@gmail.com>
2023-04-19 12:54:52 -0300

    alguns getters e setters adicionados

--- src/mainpack/Aluno.java
+++ src/mainpack/Aluno.java
@@ -4,29 +4,24 @@ import java.io.*;
 import java.util.HashMap;
 
 public class Aluno {
-    //id sempre deve possuir um valor único para a identificação do aluno!
-    private String id;
-    private String nome;
     private int maxId;
     File alunoCsv = new File("./alunos.csv");
 
+    //hashmap que guarda as linhas do arquivo aluno.csv
     HashMap<Integer, String> alunos = new HashMap<>();
 
-    //instancia o arquivo csv em um hashmap
+    //instancia o arquivo csv no hashmap
     public void lerCsv() {
         try (BufferedReader br = new BufferedReader(new FileReader(alunoCsv))) {
             String linha;
             while ((linha = br.readLine()) != null) {
                 String[] parte = linha.split(",");
-                //condicional que pula a primeira linha do arquivo csv para
-                //evitar parse do nome da coluna para int
+                //condicional que evita parse do nome da coluna 'id' para int
                 if(!parte[0].equals("id")) {
                     int id = Integer.parseInt(parte[0]);
                     String nome = parte[1];
+                    setMaxId(id);
                     alunos.put(id, nome);
-                    if(id > maxId) {
-                        maxId = id;
-                    }
                 }
             }
         } catch (IOException e) {
@@ -56,11 +51,21 @@ public class Aluno {
     }
 
     public void cadastrarAluno(String nome) throws IOException {
-        int newId = 0;
         FileWriter fr = new FileWriter("./alunos.csv", true);
         BufferedWriter br = new BufferedWriter(fr);
+        int newId = getNewId();
+        br.newLine();
+        br.write(newId + "," + nome);
+        alunos.put(newId, nome);
+        System.out.println(newId + " " + nome);
+        br.close();
+        //leitura para impedir que dados sejam sobrescritos ao serem alterados enquanto o código está em execução
+        lerCsv();
+    }
 
-        //procura o primeiro valor disponível para criar um id
+    //procura o primeiro valor disponível para criar um id
+    public int getNewId() {
+        int newId = 0;
         for(int i = 1; i < maxId; i++) {
             boolean unico = true;
             for(int id : alunos.keySet() ) {
@@ -76,13 +81,12 @@ public class Aluno {
         if(newId == 0) {
             newId = maxId + 1;
         }
-        br.newLine();
-        br.write(newId + "," + nome);
-        alunos.put(newId, nome);
-        System.out.println(newId + " " + nome);
-        br.close();
-        //leitura para impedir que dados sejam sobrescritos ao serem alterados enquanto o código está em execução
-        lerCsv();
+        return newId;
     }
 
+    public void setMaxId(int id) {
+        if(id > maxId) {
+            maxId = id;
+        }
+    }
 }
